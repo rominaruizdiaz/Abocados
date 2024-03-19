848625ad24e26561da20af0c8a750ee51b60cd90
Romina Ruiz <ruizdiazsanchezromina@gmail.com>
2024-03-19 21:57:50 +0100

    initialized repository

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from "vue-router";
+import HeaderVue from "./components/header/Header.vue";
+import FooterVue from "./components/footer/Footer.vue";
+</script>
+
+<template>
+	<main>
+		<HeaderVue />
+		<div>
+			<RouterView />
+		</div>
+		<FooterVue />
+	</main>
+</template>
+
+<style scoped lang="scss"></style>
