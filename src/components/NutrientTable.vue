d0b2796a383748fb3c40e9412c605c33f0a693cd
Romina Ruiz <ruizdiazsanchezromina@gmail.com>
2024-03-24 03:44:09 +0100

    added style to ingredient by id view

---
+++ src/components/NutrientTable.vue
@@ -0,0 +1,103 @@
+<script setup>
+
+const props = defineProps({
+  ingredient: Object,
+});
+
+</script>
+
+<template>
+    <section>
+            <div class="title">
+                <p>Tamaño de porción</p>
+                <p>{{ ingredient.weight }} {{ ingredient.unit }}</p>
+            </div>
+            <div>
+                <p class="fact">Calorías</p>
+                <p>{{ ingredient.calories }} kcal</p>
+            </div>
+            <div>
+                <p class="fact">Grasa</p>
+                <p>{{ ingredient.fats }} g</p>
+            </div>
+            <div>
+                <p class="sub-fact">Grasas saturadas</p>
+                <p>{{ ingredient.saturatedFat }} g</p>
+            </div>
+            <div>
+                <p class="sub-fact">Grasas monosaturada</p>
+                <p>{{ ingredient.monoinsaturatedFat }} g</p>
+            </div>
+            <div>
+                <p class="sub-fact">Grasas Polisaturada</p>
+                <p>{{ ingredient.polinsaturatedFat }} g</p>
+            </div>
+            <div>
+                <p class="fact">Carbohidratos</p>
+                <p>{{ ingredient.carbohydrate }} g</p>
+            </div>
+            <div>
+                <p class="sub-fact">Azúcar</p>
+                <p>{{ ingredient.sugar }} g</p>
+            </div>
+            <div>
+                <p class="sub-fact">Fibra</p>
+                <p>{{ ingredient.fiber }} g</p>
+            </div>
+            <div>
+                <p class="fact">Proteína</p>
+                <p>{{ ingredient.protein }} g</p>
+            </div>
+            <div>
+                <p class="fact">Sodio</p>
+                <p>{{ ingredient.potasio }} mg</p>
+            </div>
+            <div>
+                <p class="fact">Colesterol</p>
+                <p>{{ ingredient.fats }} mg</p>
+            </div>
+            <div class="last">
+                <p class="fact">Potasio</p>
+                <p>{{ ingredient.fats }} mg</p>
+            </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    width: 35rem;
+    height: 50rem;
+    display: grid;
+    grid-template-rows: repeat(13, 1fr);
+    background-color: #f9f9f9;
+    border-radius: 10px;
+
+    > div {
+        justify-self: center;
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+        width: 95%;
+        border-bottom: 0.1rem solid #afafaf;
+        font-weight: 500;
+    }
+}
+
+.title {
+    border-bottom: 0.8rem solid #afafaf;
+}
+.fact {
+    font-weight: bolder;
+}
+.sub-fact {
+    margin-left: 2rem;
+}
+.last {
+    border: none;
+}
+@media screen and (max-width: 750px) {
+    section {
+        width: 100%;
+    }
+}
+</style>
--- src/views/IngredientView.vue
+++ src/views/IngredientView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useIngredientsStore } from "./../stores/IngredientsStore";
 import { ref, watchEffect } from "vue";
+import NutrientTable from "./../components/NutrientTable.vue"
 
 const props = defineProps({
     id: String,
@@ -23,16 +24,72 @@ console.log("store:", ingredientStore);
 </script>
 
 <template>
-    <div v-if="ingredient">
-        <div id="image_container"></div>
-        <h1> {{ ingredient.id  }} </h1>
-    </div>
+    <main v-if="ingredient">
+        <div>
+            <div id="image_container"></div>
+            <h1>{{ ingredient.name  }}</h1>
+            <h2>{{ ingredient.category.name  }}</h2>
+        </div>
+        <article>
+            <p id="title">Tabla nutricional</p>
+            <NutrientTable :ingredient="ingredient" />
+            <p>*Ingesta de referencia de un adulto medio (2000 kcal)</p>
+        </article>
+    </main>
 </template>
 
 <style lang="scss" scoped>
+main {
+    padding: 2rem;
+    display: flex;
+    justify-content: center;
+    gap: 3rem;
+
+    > div {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 1rem;
+
+        h1 {
+            font-size: 2.3rem;
+            font-weight: 600;
+            text-align: center;
+        }
+
+        h2 {
+            font-size: 1.5rem;
+            font-weight: 500;
+        }
+    }
+
+    > article {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+    }
+}
 #image_container {
-    height: 30rem;
-    width: 100%;
+    width: 20rem;
+    height: 20rem;
+    border-radius: 10px;
     background-color: $not_found;
+}
+#title {
+    text-align: left;
+    border-bottom: 2px solid #b8b8b8;
+    height: 2rem;
+}
+@media screen and (max-width: 1000px) {
+    main {
+        flex-direction: column;
+        align-items: center;
+    }   
+}
+
+@media screen and (max-width: 750px) {
+    main {
+        flex-direction: column;
+    }   
 }
 </style>
